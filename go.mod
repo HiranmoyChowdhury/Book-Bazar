cf9da65011bfa0efdf5a4ad2499ec7426a81bacf
Hiranmoy Das Chowdhury <hiranmoy@appscode.com>
2024-01-16 19:36:14 +0600

    Cli added to the server

--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.21.5
 
 require github.com/go-chi/chi v1.5.5
 
-require github.com/oklog/ulid/v2 v2.1.0
+require (
+	github.com/oklog/ulid/v2 v2.1.0
+	github.com/spf13/cobra v1.8.0
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
---
+++ theServer/start.go
@@ -0,0 +1,20 @@
+package theServer
+
+import (
+	"github.com/go-chi/chi"
+	"learnProject/First-Project-With-Go/api"
+	"learnProject/First-Project-With-Go/utils"
+	"net/http"
+)
+
+func Start() {
+	r := chi.NewRouter()
+
+	//api.Book calls all api related to book
+	api.Book(r)
+
+	err := http.ListenAndServe(utils.GetPort(), r)
+	if err != nil {
+		return
+	}
+}
